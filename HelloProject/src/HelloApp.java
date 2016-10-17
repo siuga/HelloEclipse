0ff58d9a0eccecbb6ea30bc7696e3191b9cf3aad
siuga <siuga@unb.ca>
2016-10-17 18:51:17 -0300

    message2

--- HelloProject/src/HelloApp.java
+++ HelloProject/src/HelloApp.java
@@ -3,7 +3,7 @@ public class HelloApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sigua");
+		System.out.println("Stefan Ciprian Iuga");
 	}
 
 }
